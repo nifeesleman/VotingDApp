b8cdd8dd2306ee3ef7c2046e1e3ca33dd9f575d1
Nife Esleman <nsr-1868-14@amu.edu.et>
2025-12-21 22:34:18 +0300

    fix: correct import path for OpenZeppelin contracts and update VoterCreated event syntax

--- contracts/VotingContract.sol
+++ contracts/VotingContract.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import " @openzepplin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
 contract Create {
@@ -51,7 +51,7 @@ contract Create {
         uint256 voter_vote;
         string voter_ipfs;
     }
-    event VoterCreated{
+    event VoterCreated(
         uint256 indexed voter_voterId,
         string voter_name,
         string voter_image,
@@ -60,7 +60,7 @@ contract Create {
         bool voter_voted,
         uint256 voter_vote,
         string voter_ipfs
-    }
+    )
 
     //----------END OF VOTER DATA
 
@@ -71,5 +71,32 @@ contract Create {
     function setCandidate(address _address,string memory _age, string memory _name,string memory _image,string memory _ipfs )  returns () {
         require(votingOrganizer == msg.sender, "Only organizer can create candidates");
         _candidateId.increment();
+
+        uint256 idNumber = _candidateId.current();
+
+        Candidate storage candidate = candidates[_address];
+        candidate.age = _age;
+        candidate.name = _name;
+        candidate.candidateId = idNumber;
+        candidate.image = _image;
+        candidate.voteCount = 0;
+        candidate._address = _address;
+        candidate.ipfs = _ipfs;
+
+        candidateAddresses.push(_address);
+
+        emit CandidateCreate(
+            idNumber,
+            _age,
+            _name,
+            _image,
+            candidate.voteCount,
+            _address,
+            _ipfs
+        );
+    }
+
+    function getCandidate() public view returns (address[] memory){
+        
     }
 }
