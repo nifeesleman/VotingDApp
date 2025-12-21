6508aa65b19cc946ac152f217229710520288b95
Nife Esleman <nsr-1868-14@amu.edu.et>
2025-12-21 20:59:40 +0300

    chore: update package.json with new devDependencies and enhance globals.css with gradient background

---
+++ contracts/VotingContract.sol
@@ -0,0 +1,50 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import " @openzepplin/contracts/utils/Counters.sol";
+import "hardhat/console.sol";
+
+contract Create {
+    using Counters for Counters.Counter;
+    Counters.Counter public _voterId;
+    Counters.Counter public _candidateId;
+
+    address public VotingOrganizer;
+
+    //CANDIDATE FOR VOTING
+    struct Candidate {
+        uint256 candidateId;
+        string age;
+        string name;
+        string image;
+        uint256 voteCount;
+        address _address;
+        string ipfs;
+    }
+
+    event CandidateCreate(
+        uint256 indexed candidateId,
+        string age,
+        string name,
+        string image,
+        uint256 voteCount,
+        address _address,
+        string ipfs
+    );
+
+    address[] public candidateAddresses;
+    mapping(address => Candidate) public candidates;
+
+    /////////END OF CANDIDATE///////
+    //-------VOTER DATA
+    address[] public votedVoters;
+    address[] public voterSAddresses;
+    mapping(address => Voter) public voters;
+
+    uint 256 voter_voterId;
+    string voter_name;
+    string voter_image;
+    address voter_address;
+    uint256 voter_allowed;
+    bool 
+}
