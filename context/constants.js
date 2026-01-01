8400ebb76b34b3fb6be5da174d9ea2cf54594b74
Nife Esleman <nsr-1868-14@amu.edu.et>
2026-01-01 11:49:09 +0300

    Fix contract address in constants.js to resolve 'Connected to contract at: undefined' error

--- context/constants.js
+++ context/constants.js
@@ -1,6 +1,6 @@
 ///0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0
 
-import voting from "./Create.json";
+import Create from "./Create.json";
 
-export const VotingAddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
-export const VotingABI = voting.abi;
+export const VotingAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+export const VotingABI = Create.abi;
